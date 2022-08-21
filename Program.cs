33ba384f12a6eb5450067fe1542835e47042ea70
lexvonghost <lex_von_tost@mail.ru>
2022-08-21 20:50:49 +0300

    feat: создание массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите число, определяющее длинну массива");
+int length =  Convert.ToInt32(Console.ReadLine());
+int[] NewArray(int[] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+
+    return array;
+}
+int[] array = new int[length];
+array = NewArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
