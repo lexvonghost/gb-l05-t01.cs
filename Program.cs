b68843224ae6998f2f6b50fae527b3f68303fe71
lexvonghost <lex_von_tost@mail.ru>
2022-08-21 23:26:26 +0300

    fix: исправление ошибок , решение

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
 Console.WriteLine("Введите число, определяющее длинну массива");
-int length =  Convert.ToInt32(Console.ReadLine());
+int length = Convert.ToInt32(Console.ReadLine());
 int[] NewArray(int[] array)
 {
     for (int i = 0; i < length; i++)
@@ -16,13 +16,19 @@ int[] array = new int[length];
 array = NewArray(array);
 Console.WriteLine("[" + string.Join(", ", array) + "]");
 
-int Count = 0;
+int count = 0;
 for (int i = 0; i < length; i++)
-if (i < length)
 {
-     if (i % 2 ==0)
-        Count ++; 
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }    
 }
-    else
-        Console.WriteLine("Чётные числа не найдены");  
-Console.WriteLine("Kоличество чётных чисел в массиве:" + Count);
+if (count == 0)
+{
+    Console.WriteLine("Чётные числа не найдены");
+}
+else
+{
+Console.WriteLine("Kоличество чётных чисел в массиве:" + count);
+}
